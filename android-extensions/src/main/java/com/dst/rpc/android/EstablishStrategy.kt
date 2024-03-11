2f7f520260c0b4e957cd39ec9f30b850e79c6362
liuzhongao <liuzhongao@corp.netease.com>
2024-03-11 18:16:50 +0800

    fill comments.

--- android-extensions/src/main/java/com/dst/rpc/android/EstablishStrategy.kt
+++ android-extensions/src/main/java/com/dst/rpc/android/EstablishStrategy.kt
@@ -1,10 +1,16 @@
 package com.dst.rpc.android
 
+import com.dst.rpc.InitConfig
+
 /**
  * connection type for android rpc extensions.
  */
 enum class EstablishStrategy {
     ContentProvider,
+    /**
+     * if you choose [EstablishStrategy.Service] as your connection strategy,
+     * you must call [InitConfig.Builder.remoteAndroidServiceClass] to let [AIDLConnector] know who the target is.
+     */
     Service,
     BroadcastReceiver
 }
